f2b3729359f5209e10e6138f32ab9e13c505715a
joek-yo <jkihiu129@gmail.com>
2024-09-29 20:30:39 +0300

    Initial commit - Set up Vite with React and Bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// File: src/App.jsx
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1>Corruption Reporting App</h1>
+      <button className="btn btn-primary">Report Corruption</button>
+    </div>
+  );
+}
+
+export default App;
